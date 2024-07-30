53f5a206ad98a9a732c4f684b11b6c4a38b54fd9
梁怀刚 <lianghuaigang@xiaoduotech.com>
2024-07-30 13:32:48 +0800

    feat: 添加页面路由

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,53 @@
-import { useState, useEffect } from "react";
-import { getQueryParam, updateQueryParam } from "./utils";
-import Header from "./components/Header";
-import List from "./components/List";
+import { Routes, Route, Outlet, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Battle from "./pages/Battle";
 
-function App() {
-  const languages = ["all", "javascript", "ruby", "java", "css", "python"];
-  const getLanguage = () => {
-    const language = getQueryParam("language");
-    if (language && languages.includes(language)) {
-      return language;
-    }
-    return languages[0];
-  };
-  const [language, setLanguage] = useState(getLanguage());
-  useEffect(() => {
-    updateQueryParam("language", language);
-  }, [language]);
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="battle" element={<Battle />} />
+        {/* Using path="*"" means "match anything", so this route
+                acts like a catch-all for URLs that we don't have explicit
+                routes for. */}
+        <Route path="*" element={<NoMatch />} />
+      </Route>
+    </Routes>
+  );
+};
+
+function Layout() {
   return (
     <div>
-      <Header
-        language={language}
-        languages={languages}
-        setLanguage={setLanguage}
-      />
-      {languages.map((item) => (
-        <List key={item} language={item} show={language === item} />
-      ))}
+      {/* A "layout route" is a good place to put markup you want to
+            share across all the pages on your site, like navigation. */}
+      <nav className="container mx-auto">
+        <ul className="flex gap-3 leading-9">
+          <li>
+            <Link to="/">Popular</Link>
+          </li>
+          <li>
+            <Link to="/battle">Battle</Link>
+          </li>
+        </ul>
+      </nav>
+
+      {/* An <Outlet> renders whatever child route is currently active,
+            so you can think about this <Outlet> as a placeholder for
+            the child routes we defined above. */}
+      <Outlet />
+    </div>
+  );
+}
+
+function NoMatch() {
+  return (
+    <div>
+      <h2>Nothing to see here!</h2>
+      <p>
+        <Link to="/">Go to the home page</Link>
+      </p>
     </div>
   );
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+import { getQueryParam, updateQueryParam } from "../utils";
+import Header from "../components/Header";
+import List from "../components/List";
+
+function App() {
+  const languages = ["all", "javascript", "ruby", "java", "css", "python"];
+  const getLanguage = () => {
+    const language = getQueryParam("language");
+    if (language && languages.includes(language)) {
+      return language;
+    }
+    return languages[0];
+  };
+  const [language, setLanguage] = useState(getLanguage());
+  useEffect(() => {
+    updateQueryParam("language", language);
+  }, [language]);
+  return (
+    <div>
+      <Header
+        language={language}
+        languages={languages}
+        setLanguage={setLanguage}
+      />
+      {languages.map((item) => (
+        <List key={item} language={item} show={language === item} />
+      ))}
+    </div>
+  );
+}
+
+export default App;
